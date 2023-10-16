c52bb7a9ce3ad4cd4afc4419593a9308da5c65cc
SupanutPetch <138608190+SupanutPetch@users.noreply.github.com>
2023-10-16 22:18:41 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,21 @@
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:rechat_firebase/view/login_page.dart';
+
 import 'package:sizer/sizer.dart';
 
 import 'firebase_options.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(GetMaterialApp(home: LoginPage()));
+  FlutterNativeSplash.remove();
 }
 
 class MyApp extends StatelessWidget {
